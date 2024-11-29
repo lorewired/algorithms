ef3acbcd4f091d357b7fed76b664e0dc4dc370d5
lorewired <lorecode12@gmail.com>
2024-11-29 13:06:03 -0300

    updates

--- tests.c
+++ tests.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 
 signed main() {
-  char ch='s', secreto, resp;
-  int tentativas=1;
-  while(ch=='s'){
-    secreto = rand() % 26 + 'a';
-    printf("\n\nDigite uma letra entre 'a' e 'z':\n");
-    while((resp=getch())!=secreto){
-      printf("%c eh incorreto. Tente novamente\n", resp);
-      tentativas++;
-    }
-    printf("%c EH CORRETO!!\n", resp);
-    printf("Voce acertou em %d tentativas\n", tentativas);
-    printf("\nQuer jogar novamente? (s/n): ");
-    ch=getche();
-  }
+  
   return 0;
 }
--- tests.cpp
+++ tests.cpp
@@ -1,32 +1,13 @@
 #include <bits/stdc++.h>
+#define int long long
+#define f first
+#define s second
+#define pb push_back
+#define sz(x) (int)x.size()
 using namespace std;
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    stack<string>reverse;
-    int b;
-    cin >> b;
-    for(int c=0; c <= b;c++){
-		int t=0;
-		string a, palavra="";
-		getline(cin,a);
-		for(int i=0;i <= int(a.size());i++){
-			if(a[i] != ' ' && a[i] != '\0')
-			palavra+=a[i];
-			else{
-				reverse.push(palavra);
-				palavra="";
-				t++;
-			}
-		}
-		while(t > 0){
-			if(!reverse.empty()){
-				cout << reverse.top();
-				cout << ( t > 1 ? " " : "\0");
-				reverse.pop();
-			}
-			t--;
-		}
-		cout << "\n";
-	}
+
+signed main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
 }
