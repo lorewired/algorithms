1dd21a3a166a5ece4b410af48bff96be8a36ebfd
Lore Wired <lorecode12@gmail.com>
2024-12-06 14:55:14 -0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,37 +1,50 @@
 #include <bits/stdc++.h>
+#define f first
+#define s second
+#define pb push_back
+#define pi pair<int, int>
+#define bg begin()
+#define en end()
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
 using namespace std;
-int main() {
-  int caso, size;
-  cin >> caso;
-  while(caso--){
-    int maior=0, tam=0, seg=0, number[3], total=0;
-    queue<int> que;
-    cin >> size;
-    int num[size];
-    for(int cal=0; cal < size; cal++){
-      cin >> num[cal];
-      que.push(num[cal]);
-      if(num[cal] > maior){
-        maior=num[cal];
-        number[0] = maior;
-      }
-  } for(int cal=0; cal < size; cal++){
-    if(num[cal] < maior && num[cal] > seg){
-        seg = num[cal];
-        number[1] = seg;
-      }
-  } while(!que.empty()){
-    if(que.front() == number[0]){
-      total = que.front() - number[1];
-      cout << total << ' ';
-      total=0;
-      que.pop();
-    } else {
-      total = que.front() - number[0];
-      cout << total << ' ';
-      total=0;
-      que.pop();
+
+signed main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n,m,x;
+  cin>>n>>m;
+  vector<int> v1(n,0),v2(n,0),v3(n,0);
+  for(int i=0; i<n; i++){
+    cin>>x;
+    switch(x){
+    case 1: v1[i]=x; break;
+    case 2: v2[i]=x; break;
+    case 3: v3[i]=x; break;
     }
-   } cout << '\n';
   }
+  auto ps=[](vector<int>& v) -> vector<int>{
+    int s=sz(v);
+    vector<int> tmp(s);
+    tmp[0]=v[0];
+    for(int i=1; i<s; i++) tmp[i]=v[i]+tmp[i-1];
+    return tmp;
+  };
+  vector<int> pref1=ps(v1),pref2=ps(v2),pref3=ps(v3);
+  for(int i : v1) cout<<i<<' ';
+  cout<<'\n';
+  for(int i : v2) cout<<i<<' ';
+  cout<<'\n';
+  for(int i : v3) cout<<i<<' ';
+  cout<<'\n';
+  return 0;
+  while(m--){
+    int l,r;
+    cin>>l>>r;
+    l--; r--;
+    cout<<(!l ? pref1[r] : pref1[r]-pref1[l-1])<<' ';
+    cout<<(!l ? pref2[r] : pref2[r]-pref2[l-1])<<' ';
+    cout<<(!l ? pref3[r] : pref3[r]-pref3[l-1])<<'\n';
+  }
+  return 0;
 }
