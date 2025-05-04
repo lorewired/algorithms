ee34fec4e6f65b30c2c4863a91ec0ae4efae8117
lorewired <lorewired33@gmail.com>
2025-05-04 20:54:24 -0300

    update

--- toast.cpp
+++ toast.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 #define V vector
 #define int long long
 #define _ cin.tie(0)->sync_with_stdio(0);
 
-const int M = 1e9+7, maxN = 100005;
+using namespace std;
+const int MOD = 1e9+7;
+const int oo = 0x3f3f3f3f;
 
 signed main() { _
   
