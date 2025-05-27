fc1bae0200990b3dc544fc73553765ee871254ad
lorewired <lorecode12@gmail.com>
2025-05-26 23:55:55 -0300

    update

--- toast.cpp
+++ toast.cpp
@@ -6,39 +6,8 @@
 
 using namespace std;
 const int MOD = 1e9+7;
-const int oo = 0x3f3f3f3f;
 
 signed main() { _
-  int N, M; cin >> N >> M;
-  V<V<pair<int, int>>> graph(N);
-  while(M--) {
-    int x, y, z; cin >> x >> y >> z, x--, y--;
-    graph[x].emplace_back(z, y);
-    graph[y].emplace_back(z, x);
-  }
-  function<V<int>(int)> f = [&] (int s) -> V<int> {
-    priority_queue<pair<int, int>, V<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.emplace(0, s);
-    V<int> dist(N, oo);
-    dist[s] = 0;
-    while(pq.size()) {
-      auto [w, v] = pq.top(); pq.pop();
-      if(w > dist[v]) continue;
-      for(auto [uw, u] : graph[v]) {
-        if(w + uw < dist[u]) {
-          pq.emplace(dist[u] = w + uw, u);
-        }
-      }
-    }
-    return dist;
-  };
-  auto dist1 = f(0), dist2 = f(N-1);
-  int ans = -oo;
-  for(int i = 0; i < N; i++) {
-    if(dist1[i] != oo && dist2[i] != oo) {
-      ans = max(ans, dist1[i] + dist2[i]);
-    }
-  }
-  cout << ans;
+
   exit(0);
 }
